134e4cb1acc852526c5277f3c664e0ae310c37b1
sachin bijalwan <2015csb1027@iitrpr.ac.in>
2017-11-20 21:58:06 +0530

    FIrst commit

---
+++ clustering.m
@@ -0,0 +1,14 @@
+clc;
+clear all;
+number_of_clusters=15;
+file=importdata('label.txt',',');
+idx=kmeans(file,number_of_clusters);
+i=idx(1);
+
+for j=1:size(idx,1)
+    if(i~=idx(j))
+        fprintf('cluster %d ends at %d \n',i,j);
+        fprintf('new cluster %d begins\n',idx(j));
+        i=idx(j);
+    end
+end
