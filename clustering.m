9ca2f6818388e706e4eca7378faaf6a24038d62c
sachin bijalwan <2015csb1027@iitrpr.ac.in>
2017-11-20 22:25:51 +0530

    branching is going to be there again

--- clustering.m
+++ clustering.m
@@ -5,10 +5,18 @@ file=importdata('data.txt',',');
 idx=kmeans(file,number_of_clusters);
 i=idx(1);
 
-for j=1:size(idx,1)
-    if(i~=idx(j))
-        fprintf('cluster %d ends at %d \n',i,j);
-        fprintf('new cluster %d begins\n',idx(j));
-        i=idx(j);
+% for j=1:size(idx,1)
+%     if(i~=idx(j))
+%         fprintf('cluster %d ends at %d \n',i,j);
+%         fprintf('new cluster %d begins\n',idx(j));
+%         i=idx(j);
+%     end
+% end
+for j=1:500:5000
+    fprintf('loop %d \n',floor(j/500));
+    C=unique(idx);
+    for i=1:size(C,1)
+        fprintf('cluster %d number %d \n',C(i),sum(idx(j:j+499)==C(i)));
     end
+    
 end
